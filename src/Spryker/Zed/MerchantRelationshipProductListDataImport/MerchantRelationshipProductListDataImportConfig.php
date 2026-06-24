8045c8bf80d66fa7f26646c047fab6565fde28d4
René Klatt <rene.klatt@spryker.com>
2026-06-24 10:11:26 +0200

    Updated doc blocks (#581)
    * Updated doc blocks

--- src/Spryker/Zed/MerchantRelationshipProductListDataImport/MerchantRelationshipProductListDataImportConfig.php
+++ src/Spryker/Zed/MerchantRelationshipProductListDataImport/MerchantRelationshipProductListDataImportConfig.php
@@ -13,6 +13,8 @@ use Spryker\Zed\DataImport\DataImportConfig;
 class MerchantRelationshipProductListDataImportConfig extends DataImportConfig
 {
     /**
+     * @api
+     *
      * @var string
      */
     public const IMPORT_TYPE_MERCHANT_RELATIONSHIP_PRODUCT_LIST = 'merchant-relationship-product-list';
